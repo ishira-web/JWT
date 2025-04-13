3075a3d7122ab3206d727c30d8d6a9ad350929a5
pahas <ishira.pahasara99@gmail.com>
2025-04-13 07:57:32 +0530

    Contex updated

---
+++ Models/userModel.js
@@ -0,0 +1,44 @@
+import { boolean, number, required } from "joi";
+import mongoose, { model } from "mongoose";
+
+const userSchema = mongoose.Schema({
+
+    email :{
+        typeof : String,
+        required : [true,"Email is Required !"],
+        unique : [true,"Email must be unique !"],
+        trim :true,
+        minLenght : [5,"Email must have 5 characters !"],
+        lowercase : true,
+    },
+    password : {
+        typeof : String,
+        required : [true,"Password must be Required !"],
+        trim : true,
+        select : false
+    },
+    verified: {
+        typeof : boolean,
+        default : false
+    },
+    verificatioCode : {
+        typeof : String,
+        select : false
+    },
+    verificatioCodeValidation : {
+        typeof : String,
+        select : false
+    },
+    fogotPassCode : {
+        typeof : String,
+        select : false
+    },
+    fogotPassCodeValidation : {
+        typeof : number,
+        select : false
+    }
+},{
+    timestamps : true
+});
+
+module.exports = mongoose.model("User",userSchema);
---
+++ index.js
@@ -0,0 +1,31 @@
+import express from 'express';
+import helmet from 'helmet';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+import mongoose from 'mongoose';
+import 'dotenv/config';
+
+
+
+const app =  express()
+const PORT = 3000
+
+app.use(express.json());
+app.use(cors());
+app.use(helmet());
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+mongoose.connect(process.env.MONGODB_URI).then(()=>{
+    console.log("Database Connected !")
+}).catch((err)=>{
+    console.log(err)
+});
+
+app.get('/',(req,res)=>{
+    res.json({message : "Hello from server"})
+});
+
+
+app.listen(PORT,()=>console.log(`Listening at ${PORT}.....`));
